cb8704c26412f80c1b586e1a7ffb6f7a7b15f1ac
Iván Iglesias <iic0004@alu.ubu.es>
2018-03-15 11:10:23 +0100

    #4 #6 Comentar código y adaptar a estándar

--- Prototipos/app-dash.py
+++ Prototipos/app-dash.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from werkzeug.utils import secure_filename
@@ -9,35 +9,66 @@ import dash_html_components as html
 import pandas as pd
 import cufflinks as cf
 
+
 class CsvForm(FlaskForm):
-    file = FileField(validators=[FileRequired(), FileAllowed(['csv'], 'Solo ficheros .csv')])
+    file = FileField(validators=[FileRequired(),
+                                 FileAllowed(['csv'], 'Solo ficheros .csv')]
+                     )
+    """Form for handling file upload.
+
+    Attributes
+    ----------
+    file : FileField
+        Two validators attached, FileRequired and FileAllowed (csv format)
+    """
+
 
 server = Flask(__name__)
 
 server.config['SECRET_KEY'] = 'secret key'
+server.config['UPLOAD_FOLDER'] = 'csvs'
 
 app = dash.Dash(__name__, server=server, url_base_pathname='/dash_app')
 app.title = 'Aplicación Dash'
-app.layout = html.Div()
+app.layout = html.Div()  # Needs a layout, so empty layout is good to go
 
 cf.go_offline()
+
 
 @server.route('/')
 @server.route('/index')
 def index():
+    """Method for the index view.
+
+    Just renders the index template.
+    """
     return render_template('index-dash.html')
 
 
 @server.route('/upload', methods=['GET', 'POST'])
 def upload():
+    """Method for the upload view.
+
+    This view contains a form for file uploading. If the request is POST,
+    validates the selected file and saves it. Then pandas reads it and the Dash
+    layout gets defined. The redirect to the Dash route.
+    """
     form = CsvForm()
     if form.validate_on_submit():
         f = form.file.data
         filename = secure_filename(f.filename)
-        f.save(os.path.join('csvs', filename))
-        data = pd.read_csv('{}/{}'.format('csvs',filename), sep=';', header=None)
-        data.columns = ['longitud','valor']
-        fig = data.iplot(x='longitud', y='valor', kind='scatter', asFigure=True, title='Datos de ejemplo', xTitle='Longitud de onda', yTitle='Valor')
+        directory = os.path.join(server.instance_path,
+                                 server.config['UPLOAD_FOLDER']
+                                 )
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        f.save(os.path.join(directory, filename))
+        data = pd.read_csv(f'{directory}/{filename}', sep=';', header=None)
+        data.columns = ['Raman shift', 'Intensity']
+        fig = data.iplot(x='Raman shift', y='Intensity', asFigure=True,
+                         title='Datos de ejemplo', xTitle='Longitud de onda',
+                         yTitle='Valor'
+                         )
         app.layout = html.Div([
             html.H1('Aplicación Dash'),
 
@@ -50,6 +81,5 @@ def upload():
     return render_template('upload.html', form=form)
 
 
-
 if __name__ == '__main__':
-    server.run(debug=True)
+    app.run_server(debug=True)
