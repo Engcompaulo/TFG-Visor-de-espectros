9d32bb38e9edef4533d42adc3fecabee0f27a85a
Iván Iglesias <iic0004@alu.ubu.es>
2018-04-29 14:01:34 +0200

    #34 Añadidas variables de Mongo a la configuración

--- config.py
+++ config.py
@@ -15,6 +15,21 @@ class Config(object):
     GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
     GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    UPLOAD_FOLDER_CSV = os.environ.get('UPLOAD_FOLDER_CSV')
-    UPLOAD_FOLDER_ZIP = os.environ.get('UPLOAD_FOLDER_ZIP')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class DevelopmentConfig(Config):
+    DATA_MONGODB_HOST = 'localhost'
+    MONGO_URI = f'mongodb://{DATA_MONGODB_HOST}:27017/'
+
+
+class ProductionConfig(Config):
+    DATA_MONGODB_HOST = os.environ.get('DATA_MONGODB_HOST')
+    MONGO_URI = f'mongodb://{DATA_MONGODB_HOST}:27017/'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
