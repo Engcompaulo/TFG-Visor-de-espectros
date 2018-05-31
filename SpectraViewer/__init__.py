bd54906c9de8d6919a970fd4d6d23f41bdbcb420
Iván Iglesias <iic0004@alu.ubu.es>
2018-05-31 16:41:17 +0200

    #58 Index para espectros

--- SpectraViewer/__init__.py
+++ SpectraViewer/__init__.py
@@ -66,3 +66,6 @@ def _init_extensions(app):
         mongo.db.datasets.create_index([('dataset_name', pymongo.ASCENDING),
                                         ('user_id', pymongo.ASCENDING)],
                                        unique=True)
+        mongo.db.spectra.create_index([('spectrum_name', pymongo.ASCENDING),
+                                       ('user_id', pymongo.ASCENDING)],
+                                      unique=True)
