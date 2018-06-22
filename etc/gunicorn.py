2a7b92cfebc3e1be30a2820144f95857e94dba00
Iván Iglesias <iic0004@alu.ubu.es>
2018-06-22 11:58:36 +0200

    Por si acaso tarda mucho

--- etc/gunicorn.py
+++ etc/gunicorn.py
@@ -67,7 +67,7 @@ access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"
 workers = 2
 worker_class = 'sync'
 worker_connections = 1000
-timeout = 120
+timeout = 900
 keepalive = 2
 
 spew = False
